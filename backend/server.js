0d8ba4af7874ce9798b32888f69cb39ab62faeef
Hongli Wang <1023836798@qq.com>
2020-09-02 09:44:58 -0400

    add api for product

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,18 @@ import data from './data';
 
 const app = express();
 
+app.get("/api/products:id", (req,res)=>{
+    const productId = req.params.id;
+    res.send(data.products.find(x => x._id === productId));
+    if(product){
+        res.send(product);
+    }else{
+        res.status(404).send({msg: "Product Not Found."});
+    }
+});
+
 app.get("/api/products", (req,res)=>{
     res.send(data.products);
 });
+
 app.listen(5000, () => {console.log("Server started at http://localhost:5000")});
--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,15 +1,28 @@
-import React from "react";
-import data from "../data";
+import React, { useEffect } from "react";
 import {Link} from 'react-router-dom';
+import {useDispatch, useSelector } from 'react-redux';
+import { detailsProduct } from "../actions/productActions";
 
 function ProductScreen(props) {
-  const product = data.products.find((x) => x._id === props.match.params.id);
+    const productDetails = useSelector(state => state.productDetails);
+    const {product, loading, error} = productDetails;
+    const dispatch = useDispatch;
+    useEffect(() => {
+        dispatch(detailsProduct());
+        return () => {
+            //
+        }
+    }, [])
+
   return (
     <div>
         <div className="back-to-result">
             <Link to="/">Back to result</Link>
         </div>
-        <div className="details">
+        {loading ? <div>loading...</div>:
+        error ? <div>{error}</div>:
+        (
+            <div className="details">
             <div className="details-image">
                 <img src={product.image} alt="product"/>
             </div>
@@ -60,8 +73,10 @@ function ProductScreen(props) {
                 </ul>
             </div>
         </div>
+        )
+        }
     </div>
-  );
+    );
 }
 
 export default ProductScreen;
