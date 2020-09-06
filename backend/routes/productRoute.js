e2387ee217a889564bcc82e772bf536adcb70040
Hongli Wang <1023836798@qq.com>
2020-09-05 21:01:18 -0400

    Fix bugs for product details

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -8,6 +8,14 @@ router.get("/", async (req,res) => {
     const products = await Product.find({});
     res.send(products);
 });
+router.get('/:id', async (req, res) => {
+    const product = await Product.findOne({ _id: req.params.id });
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404).send({ message: 'Product Not Found.' });
+    }
+  });
 
 router.post("/", isAuth, isAdmin, async (req,res) => {
     const product = new Product({
