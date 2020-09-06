ed3cff47a44a007d35177438452a03a6cc37d33c
Hongli Wang <1023836798@qq.com>
2020-09-05 21:43:52 -0400

    Finish shipping screen

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -1,68 +1,82 @@
-import React from 'react';
+import React from "react";
 
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { saveShipping } from '../actions/cartAction';
+import { saveShipping } from "../actions/cartAction";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 function ShippingScreen(props) {
-    const [address,setAddress] = useState('');
-    const [postalCode, setPostalCode] = useState('');
-    const [city, setCity] = useState('');
-    const [country, setCountry] = useState('');
+  const [address, setAddress] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(saveShipping(address, city, postalCode, country));
+    props.history.push('payment');
+  };
+  return (
+    <div>
+      <CheckoutSteps step1 step2></CheckoutSteps>
+      <div className="form">
+        <form onSubmit={submitHandler}>
+          <ul className="form-container">
+            <li>
+              <h2>Shipping</h2>
+            </li>
+            <li>
+              <label htmlFor="address">Address</label>
+              <input
+                type="name"
+                name="address"
+                id="address"
+                onChange={(e) => setAddress(e.target.value)}
+              ></input>
+            </li>
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-        dispatch(saveShipping(address,city,postalCode,country));
-    }
-    return (
-        <div className="form">
-    <form onSubmit={submitHandler} >
-      <ul className="form-container">
-        <li>
-          <h2>Shipping</h2>
-        </li>
-        <li>
-          <label htmlFor="address">
-            Address
-          </label>
-          <input type="name" name="address" id="address" onChange={(e) => setAddress(e.target.value)}>
-          </input>
-        </li>
+            <li>
+              <label htmlFor="city">City</label>
+              <input
+                type="name"
+                name="city"
+                id="city"
+                onChange={(e) => setCity(e.target.value)}
+              ></input>
+            </li>
 
-        <li>
-          <label htmlFor="city">
-            City
-          </label>
-          <input type="name" name="city" id="city" onChange={(e) => setCity(e.target.value)}>
-          </input>
-        </li>
+            <li>
+              <label htmlFor="country">Country</label>
+              <input
+                type="name"
+                name="country"
+                id="country"
+                onChange={(e) => setCountry(e.target.value)}
+              ></input>
+            </li>
 
-        <li>
-          <label htmlFor="country">
-            Country
-          </label>
-          <input type="name" name="country" id="country" onChange={(e) => setCountry(e.target.value)}>
-          </input>
-        </li>
-
-        <li>
-          <label htmlFor="postalCode">
-            Zip Code
-          </label>
-          <input type="name" name="postalCode" id="postalCode" onChange={(e) => setPostalCode(e.target.value)}>
-          </input>
-        </li>
-        <li>
-          <button type="submit" className="button primary">Continue</button>
-        </li>
-      </ul>
-    </form>
-  </div>
-    )
+            <li>
+              <label htmlFor="postalCode">Zip Code</label>
+              <input
+                type="name"
+                name="postalCode"
+                id="postalCode"
+                onChange={(e) => setPostalCode(e.target.value)}
+              ></input>
+            </li>
+            <li>
+              <button type="submit" className="button primary">
+                Continue
+              </button>
+            </li>
+          </ul>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default ShippingScreen;
