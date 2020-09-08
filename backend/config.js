37cd7b277411c9e3b16ddcf1a97edd1f88e13320
Hongli Wang <1023836798@qq.com>
2020-09-08 10:15:51 -0400

    deploy

--- backend/config.js
+++ backend/config.js
@@ -4,5 +4,6 @@ dotenv.config();
 export default{
     MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/Ecomozon',
     JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
-    PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb'
+    PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
+    PORT: process.env.PORT || 5000
 }
--- backend/server.js
+++ backend/server.js
@@ -25,17 +25,8 @@ app.get("/api/config/paypal", (req,res) => {
     res.send(config.PAYPAL_CLIENT_ID);
 })
 
-// app.get("/api/products/:id", (req,res)=>{
-//     const productId = req.params.id;
-//     const product = data.products.find(x => x._id === productId);
-//     if(product){
-//         res.send(product);
-//     }else{
-//         res.status(404).send({msg: "Product Not Found."});
-//     }
-// });
-// app.get("/api/products", (req,res)=>{
-//     res.send(data.products);
-// });
+app.use(express.static(path.join(__dirname, ‘/../frontend/build’)));
 
-app.listen(5000, () => {console.log("Server started at http://localhost:5000")});
+app.get('*', (req, res) => res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+
+app.listen(config.PORT, …)
